0dbf25f47828e2d4b8d7b482eeb61fa5c4baeeef
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-05-24 20:46:12 -0600

    Se añaden los modulos de State y Task al modulo principal app

--- task-manager/server-nest/src/app.module.ts
+++ task-manager/server-nest/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
 import { StateController } from './state/state.controller';
 import { StateService } from './state/state.service';
 import { StateModule } from './state/state.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, StateModule],
+  imports: [UsersModule, AuthModule, StateModule, TasksModule],
   controllers: [StateController],
   providers: [StateService],
 })
