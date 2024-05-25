5227f97ea6290376e72558e332f4e2dc401f8b59
ivaniuss <ivan.emi94@gmail.com>
2024-05-25 10:19:54 -0500

    login made public

--- task-manager/server-nest/src/auth/auth.controller.ts
+++ task-manager/server-nest/src/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
