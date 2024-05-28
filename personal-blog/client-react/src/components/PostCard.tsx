39a6c79f1b16ee2fd26de574c890f7b235cddf7c
Tron89 <126179079+Tron89@users.noreply.github.com>
2024-05-28 16:24:57 +0200

    feat: the blogs are get of the server (#22) (#26)
    Co-authored-by: Tron89 <claudiuacricta@gmail.com>

--- personal-blog/client-react/src/components/PostCard.tsx
+++ personal-blog/client-react/src/components/PostCard.tsx
@@ -1,19 +1,26 @@
-import image from '../img/ofice.webp'
+import { format } from "date-fns";
 
 function PostCard(props) {
-    return (
+  let time = "";
+  if (props.time != undefined)
+    time = format(props.time, "MMMM dd, yyyy - HH:mm");
+
+  return (
     <a href="">
       <div className="max-md:max-w-96 md:max-w-80  bg-slate-800 text-center max-md:h-[30rem] md:h-[26rem] rounded-3xl overflow-hidden hover:-translate-y-6 transition">
-        <img src={props.image} alt="" className='mb-5'/>
-        <div className='px-5'>
-        <h3 className="max-md:text-2xl md:text-lg mb-1">{props.title}</h3>
-        <h4 className="max-md:text-lg md:text-base text-slate-500 mb-3">{props.time}</h4>
-        <p className="max-md:text-lg md:text-base break-words text-base overflow-hidden overflow-ellipsis">{props.description}</p>
+        <img src={props.image} alt="" className="mb-5" />
+        <div className="px-5">
+          <h3 className="max-md:text-2xl md:text-lg mb-1">{props.title}</h3>
+          <h4 className="max-md:text-lg md:text-base text-slate-500 mb-3">
+            {time}
+          </h4>
+          <p className="max-md:text-lg md:text-base break-words text-base overflow-hidden overflow-ellipsis">
+            {props.description}
+          </p>
         </div>
       </div>
     </a>
+  );
+}
 
-    )
-  }
-  
-  export default PostCard
+export default PostCard;
