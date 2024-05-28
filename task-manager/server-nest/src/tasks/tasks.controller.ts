1ec59243193465045dcc44f1535f99aa2baa3f23
ivaniuss <ivan.emi94@gmail.com>
2024-05-28 13:55:50 -0500

    task module changed

--- task-manager/server-nest/src/tasks/tasks.controller.ts
+++ task-manager/server-nest/src/tasks/tasks.controller.ts
@@ -30,18 +30,8 @@ export class TasksController {
   }
 
   @Post()
-<<<<<<< HEAD
-<<<<<<< HEAD
   createTask(@Request() req, @Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(req.user.id, createTaskDto);
-=======
-  createTask(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
->>>>>>> 087026b (Feature/task (#25))
-=======
-  createTask(@Request() req, @Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(req.user.id, createTaskDto);
->>>>>>> 23ea12c (Feature/task (#30))
   }
 
   @Put(':id')
