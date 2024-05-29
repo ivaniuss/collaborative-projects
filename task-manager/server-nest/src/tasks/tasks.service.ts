1b8cab5984d8a8addc02bf8f646b64170362d4e4
ivaniuss <ivan.emi94@gmail.com>
2024-05-29 18:19:44 -0500

    tasks module modified

--- task-manager/server-nest/src/tasks/tasks.service.ts
+++ task-manager/server-nest/src/tasks/tasks.service.ts
@@ -14,54 +14,12 @@ export class TasksService {
     return await prisma.task.findUnique({ where: { userId: userId, id: id } });
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b754d6c (Feature/task (#25))
-=======
->>>>>>> 148dee7 (Feature/task (#30))
   async create(userId: string, createTaskDto: CreateTaskDto) {
-=======
-  async create(createTaskDto: CreateTaskDto) {
->>>>>>> 087026b (Feature/task (#25))
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  async create(userId: string, createTaskDto: CreateTaskDto) {
->>>>>>> 23ea12c (Feature/task (#30))
-=======
->>>>>>> b754d6c (Feature/task (#25))
-=======
-=======
-  async create(userId: string, createTaskDto: CreateTaskDto) {
->>>>>>> 23ea12c (Feature/task (#30))
->>>>>>> 148dee7 (Feature/task (#30))
     return await prisma.task.create({
       data: {
         ...createTaskDto,
         update: new Date(),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 148dee7 (Feature/task (#30))
         userId: userId,
-=======
->>>>>>> 087026b (Feature/task (#25))
-=======
-        userId: userId,
->>>>>>> 23ea12c (Feature/task (#30))
-<<<<<<< HEAD
-=======
-        userId: userId,
-=======
->>>>>>> 087026b (Feature/task (#25))
->>>>>>> b754d6c (Feature/task (#25))
-=======
->>>>>>> 148dee7 (Feature/task (#30))
       },
     });
   }
