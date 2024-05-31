f39d54fde84079fb25d41db7ff4ba3536d758ca2
CarlosAVargas7 <karltfx15@outlook.com>
2024-05-30 20:24:01 -0500

    0.0

--- task-manager/client-react/src/App.tsx
+++ task-manager/client-react/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Dashboard from './Dashboard/Dashboard'
+import RegisterLogin from './RegisterLogin/RegisterLogin'; // Ajusta la ruta si es necesario
 
 function App() {
 
   return (
     <>
     <Dashboard/>
+    <RegisterLogin/>
     </>
   )
 }
