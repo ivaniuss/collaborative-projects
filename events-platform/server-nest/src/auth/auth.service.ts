74d54d6a6067b6adfe2b6d70c43d9311f40e7909
ivaniuss <ivan.emi94@gmail.com>
2024-06-02 23:55:55 -0500

    users entity folder removed

--- events-platform/server-nest/src/auth/auth.service.ts
+++ events-platform/server-nest/src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
-import { User } from 'src/users/entities/user.entity';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class AuthService {
